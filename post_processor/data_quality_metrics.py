47150e146764dec88e1cdb8b571389adfa7d8e10
Anaida <afg.gatv@gmail.com>
2023-05-22 18:06:00 +0200

    feat: simplify json and fix tests
    Clean output json and delete config input

--- post_processor/data_quality_metrics.py
+++ post_processor/data_quality_metrics.py
@@ -1,20 +1,14 @@
-from abstracts.AbstractPostProcessor import APostProcessor
 # evidently 0.1.59.dev3
 from evidently.tests import *
 from evidently.test_suite import TestSuite
-from evidently.metrics.base_metric import generate_column_metrics
 from evidently.utils import NumpyEncoder
 import json
 
 class data_quality_metrics():
-    def __init__(self, config):
-        self.config = config
-
     def execute(self, data):
-        # Parse desired tests from config
-        columns = list(data.columns)
-        tests = self.parse_test_conf(self.config, columns)
-
+        # Define tests to perform
+        tests = [TestNumberOfRows(), TestNumberOfColumns(), TestNumberOfNulls(), TestNumberOfColumnsWithNulls(),
+                 TestNumberOfRowsWithNulls(), TestNumberOfConstantColumns(), TestNumberOfEmptyColumns()]
         # Create and execute Test Suite
         custom_data_quality_test = TestSuite(tests=tests)
         custom_data_quality_test.run(current_data=data, reference_data=None)
@@ -22,36 +16,16 @@ class data_quality_metrics():
         # Convert tests output to json file
         tst_dic = custom_data_quality_test.as_dict()
         tst_json = json.loads(json.dumps(tst_dic, cls=NumpyEncoder))
+        final_dict = {}
+        tests_names = ['number_of_rows', 'number_of_columns', 'number_of_nulls', 'number_of_columns_with_nulls',
+                       'number_of_rows_with_nulls', 'number_of_constant_columns', 'number_of_empty_columns']
+        for i, test in enumerate(tst_json['tests']):
+            if len(test['parameters']) == 0:
+                final_dict[tests_names[i]] = 0
+            else:
+                final_dict[tests_names[i]] = test['parameters'][tests_names[i]]
+        return(final_dict)
 
-        return(tst_json)
-
-    def parse_test_conf(self, conf, columns):
-        # Initialize tests output and dictionary of tests
-        tests = []
-        tests_dict = {'TestNumberOfRows': TestNumberOfRows(),
-                      'TestNumberOfColumns': TestNumberOfColumns(),
-                      'TestNumberOfNulls': TestNumberOfNulls(),
-                      'TestNumberOfColumnsWithNulls': TestNumberOfColumnsWithNulls(),
-                      'TestNumberOfRowsWithNulls': TestNumberOfRowsWithNulls(),
-                      'TestColumnNumberOfNulls': generate_column_metrics(TestColumnNumberOfNulls, columns=columns),
-                      'TestNumberOfConstantColumns': TestNumberOfConstantColumns(),
-                      'TestNumberOfEmptyColumns': TestNumberOfEmptyColumns(),
-                      }
-        # Load tests included in cofig file
-        for k, v in conf.items():
-            if v == True:
-                tests.append(tests_dict[k])
-        return tests
-
-# Example of config dict:
-# config = {'TestNumberOfRows': True,
-#           'TestNumberOfColumns': True,
-#           'TestNumberOfNulls': True,
-#           'TestNumberOfColumnsWithNulls': True,
-#           'TestNumberOfRowsWithNulls': True,
-#           'TestNumberOfConstantColumns': True,
-#           'TestColumnNumberOfNulls': True,
-#           'TestNumberOfEmptyColumns': True}
 
 '''if __name__=="__main__":
     import pandas as pd
@@ -63,6 +37,6 @@ class data_quality_metrics():
     with open(config_path, 'r') as stream:
         config_dict = yaml.safe_load(stream)
     print(config_dict)
-    post_pro = data_quality_metrics(config=config_dict)
+    post_pro = data_quality_metrics()
     result = post_pro.execute(sampled_data)
     print(result)'''
